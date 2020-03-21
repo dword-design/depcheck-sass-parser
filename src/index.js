f6df4518d5eb0d212715fb8631725f98521dcd63
foo <foo@bar.de>
2020-03-21 13:32:28 +0100

    fix: upgrades

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import getPackageName from 'get-package-name'
 import requirePackageName from 'require-package-name'
-import importer from '@dword-design/node-sass-importer'
+import importer from 'node-sass-tilde-importer'
 import sass from 'node-sass'
 import { map, uniq, compact, replace } from '@dword-design/functions'
 import P from 'path'
 
 export default filePath => {
   const { stats } = sass.renderSync({ file: filePath, importer })
-
   return stats.includedFiles
     |> map(path => path[0] === '~'
       ? requirePackageName(path.substr(1))
--- src/index.spec.js
+++ src/index.spec.js
@@ -4,59 +4,6 @@ import outputFiles from 'output-files'
 import { endent } from '@dword-design/functions'
 
 export default {
-  'css import subpath': () => withLocalTmpDir(async () => {
-    await outputFiles({
-      'depcheck.config.js': endent`
-        const sassParser = require('@dword-design/depcheck-sass-parser')
-
-        module.exports = {
-          parsers: {
-            '*.scss': sassParser,
-          },
-        }
-      `,
-      'node_modules/bar/dist/index.css': '',
-      'package.json': endent`
-        {
-          "dependencies": {
-            "bar": "^1.0.0"
-          }
-        }
-      `,
-      'src/index.scss': '@import \'~bar/dist/index.css\';',
-    })
-    await execa.command('depcheck --config depcheck.config.js')
-  }),
-  'css import': () => withLocalTmpDir(async () => {
-    await outputFiles({
-      'depcheck.config.js': endent`
-        const sassParser = require('@dword-design/depcheck-sass-parser')
-
-        module.exports = {
-          parsers: {
-            '*.scss': sassParser,
-          },
-        }
-      `,
-      'node_modules/bar': {
-        'index.css': '',
-        'package.json': endent`
-          {
-            "main": "index.css"
-          }
-        `,
-      },
-      'package.json': endent`
-        {
-          "dependencies": {
-            "bar": "^1.0.0"
-          }
-        }
-      `,
-      'src/index.scss': '@import \'~bar\';',
-    })
-    await execa.command('depcheck --config ./depcheck.config.js')
-  }),
   'sass import': () => withLocalTmpDir(async () => {
     await outputFiles({
       'depcheck.config.js': endent`
